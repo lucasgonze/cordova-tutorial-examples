e1fe5d7e1201a969bec3a7f11be6997bf6f4ed8a
Lucas Gonze <lucas@gonze.com>
2014-12-02 19:38:37 -0800

    Module 13 - contacts API. Tested in ios emulator. Corrected typo "hre" to "href."

--- workshop/platforms/ios/www/cordova_plugins.js
+++ workshop/platforms/ios/www/cordova_plugins.js
@@ -62,6 +62,83 @@ module.exports = [
         "clobbers": [
             "navigator.geolocation"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/contacts.js",
+        "id": "org.apache.cordova.contacts.contacts",
+        "clobbers": [
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/Contact.js",
+        "id": "org.apache.cordova.contacts.Contact",
+        "clobbers": [
+            "Contact"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactAddress.js",
+        "id": "org.apache.cordova.contacts.ContactAddress",
+        "clobbers": [
+            "ContactAddress"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactError.js",
+        "id": "org.apache.cordova.contacts.ContactError",
+        "clobbers": [
+            "ContactError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactField.js",
+        "id": "org.apache.cordova.contacts.ContactField",
+        "clobbers": [
+            "ContactField"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactFindOptions.js",
+        "id": "org.apache.cordova.contacts.ContactFindOptions",
+        "clobbers": [
+            "ContactFindOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactName.js",
+        "id": "org.apache.cordova.contacts.ContactName",
+        "clobbers": [
+            "ContactName"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactOrganization.js",
+        "id": "org.apache.cordova.contacts.ContactOrganization",
+        "clobbers": [
+            "ContactOrganization"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactFieldType.js",
+        "id": "org.apache.cordova.contacts.ContactFieldType",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ios/contacts.js",
+        "id": "org.apache.cordova.contacts.contacts-ios",
+        "merges": [
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ios/Contact.js",
+        "id": "org.apache.cordova.contacts.Contact-iOS",
+        "merges": [
+            "Contact"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -71,7 +148,8 @@ module.exports.metadata =
     "org.apache.cordova.console": "0.2.11",
     "org.apache.cordova.dialogs": "0.2.10",
     "org.apache.cordova.statusbar": "0.1.8",
-    "org.apache.cordova.geolocation": "0.3.10"
+    "org.apache.cordova.geolocation": "0.3.10",
+    "org.apache.cordova.contacts": "0.2.14"
 }
 // BOTTOM OF METADATA
 });
--- workshop/platforms/ios/www/js/EmployeeView.js
+++ workshop/platforms/ios/www/js/EmployeeView.js
@@ -12,9 +12,27 @@ var EmployeeView = function(employee) {
 	  return false;
 	};
 
+	this.addToContacts = function(event) {
+	    event.preventDefault();
+	    console.log('addToContacts');
+	    if (!navigator.contacts) {
+	        alert("Contacts API not supported", "Error");
+	        return;
+	    }
+	    var contact = navigator.contacts.create();
+	    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+	    var phoneNumbers = [];
+	    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+	    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+	    contact.phoneNumbers = phoneNumbers;
+	    contact.save();
+	    return false;
+	};
+	
   this.initialize = function() {
       this.$el = $('<div/>');
   	  this.$el.on('click', '.add-location-btn', this.addLocation);
+	  this.$el.on('click', '.add-contact-btn', this.addToContacts);
   };
 
   this.render = function() {
