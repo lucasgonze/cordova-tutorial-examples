feb73bd60fe81d31f66202ceef70075f55c6e7c4
Lucas Gonze <lucas@gonze.com>
2014-12-02 18:19:07 -0800

    Steps 1 and 2 in Module 8.

--- workshop/www/js/app.js
+++ workshop/www/js/app.js
@@ -2,6 +2,8 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+	var homeTpl = Handlebars.compile($("#home-tpl").html());
+	var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
 	var service = new EmployeeService();
 	service.initialize().done(function () {
 	    renderHomeView();
@@ -23,25 +25,15 @@
 	}, false);
  
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    }
-
+	function findByName() {
+	    service.findByName($('.search-key').val()).done(function (employees) {
+	        $('.content').html(employeeListTpl(employees));
+	    });
+	}
+	
 	function renderHomeView() {
-	    var html =
-	      "<h1>Directory</h1>" +
-	      "<input class='search-key' type='search' placeholder='Enter name'/>" +
-	      "<ul class='employee-list'></ul>";
-	    $('body').html(html);
+	    $('body').html(homeTpl());
 	    $('.search-key').on('keyup', findByName);
 	}
-
+	
 }());
