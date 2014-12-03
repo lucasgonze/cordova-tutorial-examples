e89a04bc6b7f6a2ef951347ce0c08604d11076ae
Lucas Gonze <lucas@gonze.com>
2014-12-02 18:06:31 -0800

    $ cordova plugin add org.apache.cordova.dialogs $ cordova build ios && cordova emulate ios

--- workshop/platforms/ios/www/js/app.js
+++ workshop/platforms/ios/www/js/app.js
@@ -8,7 +8,20 @@
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
+	document.addEventListener('deviceready', function () {
+	  if (navigator.notification) { // Override default HTML alert with native dialog
+	      window.alert = function (message) {
+	          navigator.notification.alert(
+	              message,    // message
+	              null,       // callback
+	              "Workshop", // title
+	              'OK'        // buttonName
+	          );
+	      };
+	  }
+	}, false);
+ 
+   $('.search-key').on('keyup', findByName);
     $('.help-btn').on('click', function() {
         alert("Employee Directory v3.4");
     });
