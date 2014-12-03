d7c9332a15a526ee5042cc21c27542a3018be22d
Lucas Gonze <lucas@gonze.com>
2014-12-02 18:52:05 -0800

    Step 3 of Module 10.

--- workshop/platforms/ios/www/js/app.js
+++ workshop/platforms/ios/www/js/app.js
@@ -5,11 +5,22 @@
 	HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
 	EmployeeListView.prototype.template = 
 	            Handlebars.compile($("#employee-list-tpl").html());
+	EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 	var service = new EmployeeService();
+
 	service.initialize().done(function () {
-	    $('body').html(new HomeView(service).render().$el);
-	});
-		
+	  router.addRoute('', function() {
+	      $('body').html(new HomeView(service).render().$el);
+	  });
+
+	  router.addRoute('employees/:id', function(id) {
+	      service.findById(parseInt(id)).done(function(employee) {
+	          $('body').html(new EmployeeView(employee).render().$el);
+	      });
+	  });
+
+	  router.start();
+	});		
     /* --------------------------------- Event Registration -------------------------------- */
 	document.addEventListener('deviceready', function () {
 		StatusBar.overlaysWebView( false );
